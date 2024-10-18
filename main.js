71373a5b17873dde71e3fa153602f3b23993be6a
RaquelCosta07 <raquel.s.gomes.costa@gmail.com>
2024-10-18 10:21:46 +0100

    show new cat form exercise

--- main.js
+++ main.js
@@ -18,62 +18,84 @@ const kittens = document.querySelector(".js-list");
                  class="card_img"
                  src=${kittenImage1}                 alt="siames-cat"
                />
-               <h3 class="card_title">Anastacio</h3>
-               <h4 class="card_race">Siamés</h4>
+               <h3 class="card_title">${kittenName1}</h3>
+               <h4 class="card_race"> ${kittenRace1}</h4>
                <p class="card_description">
-                Porte elegante, su patrón de color tan característico y sus ojos
-                 de un azul intenso, pero su historia se remonta a Asía al menos
-                 hace 500 años, donde tuvo su origen muy posiblemente.
+                ${kittenDesc1}
                </p>
              </article>
          </li>`;
+
+        const kittenImage2 = "https://dev.adalab.es/sphynx-gato.webp";
+        const kittenName2 = "Fiona";
+        const kittenDesc2 =
+           " Produce fascinación y curiosidad. Exótico, raro, bello, extraño hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.";
+         const kittenRace2 = "Sphynx"; 
 
           const kittenTwo =
            `<li class="card">
             <img
               class="card_img"
-              src="https://dev.adalab.es/sphynx-gato.webp"
+              src="${kittenImage2}"
               alt="sphynx-cat"
             />
-            <h3 class="card_title">Fiona</h3>
-            <h4 class="card_race">Sphynx</h4>
+            <h3 class="card_title">${kittenName2}</h3>
+            <h4 class="card_race">${kittenRace2}</h4>
             <p class="card_description">
-              Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
-              hasta con pinta de alienígena han llegado a definir a esta raza
-              gatuna que se caracteriza por la «ausencia» de pelo.
+              ${kittenDesc2}
             </p>
           </li>`;
+
+        const kittenImage3 = "https://dev.adalab.es/maine-coon-cat.webp";
+        const kittenName3 = "Cielo";
+        const kittenDesc3 =
+           " Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.";
+        const kittenRace3 = "Maine Coon"; 
 
         const kittenThree = `<li class="card">
             <img
               class="card_img"
-              src="https://dev.adalab.es/maine-coon-cat.webp"
+              src=${kittenImage3}
               alt="maine-coon-cat"
             />
-            <h3 class="card_title">Cielo</h3>
-            <h4 class="card_race">Maine Coon</h4>
+            <h3 class="card_title">${kittenName3}</h3>
+            <h4 class="card_race">${kittenRace3}</h4>
             <p class="card_description">
-              Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
-              bella mirada se ha convertido en una de sus señas de identidad.
-              Sus ojos son grandes y las orejas resultan largas y en punta.
+              ${kittenDesc3}
             </p>
           </li>`;
 
 
         kittens.innerHTML = kittenOne + kittenTwo + kittenThree;
 
+    
+  
+    
     const menuAdd = document.querySelector('.js-btn-add');
+    const newForm = document.querySelector('.newForm');
 
-    menuAdd.addEventListener("click", () => {
-      const formSection = document.querySelector('.new-form');
-      formSection.classList.remove('collapsed');
-    });
+      function showNewCatForm () {
+        newForm.classList.remove('collapsed');
+        console.log(newForm)}
+    
+      function hideNewCatForm() {
+        newForm.classList.add('collapsed');
+      }
 
-    const buttonCancel = document.querySelector('.js-btn-cancel');
+    newForm.addEventListener("click", showNewCatForm);
+   
+    
+    //   formSection.classList.remove('collapsed');
+    // });
 
-    buttonCancel.addEventListener("click", () => {
-      const formSection = document.querySelector('.new-form');
-      formSection.classList.add('collapsed');
-      // console.log("clickcancel");
-    });
+    // const buttonCancel = document.querySelector('.js-btn-cancel');
 
+    // buttonCancel.addEventListener("click", () => {
+    //   const formSection = document.querySelector('.new-form');
+    //   formSection.classList.add('collapsed');
+    //   // console.log("clickcancel");
+    // });
+
+ 
+
+      
