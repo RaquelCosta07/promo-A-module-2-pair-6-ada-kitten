5fd9233cc6a14fcecbb6e4df281fe30fd74c4052
Kristhinne <krs.opie@gmail.com>
2024-10-21 11:18:48 +0200

    buscador

--- main.js
+++ main.js
@@ -3,16 +3,16 @@
 const kittens = document.querySelector(".js-list");
 
 
-         
-         const kittenImage1 = "https://dev.adalab.es/gato-siames.webp";
-         const kittenName1 = "Anastacio";
-         const kittenDesc1 =
-           " Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.";
-         const kittenRace1 = "Siamés"; 
-         
-         
-        const kittenOne = 
-        `<li class="card">
+
+const kittenImage1 = "https://dev.adalab.es/gato-siames.webp";
+const kittenName1 = "Anastacio";
+const kittenDesc1 =
+  " Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.";
+const kittenRace1 = "Siamés";
+
+
+const kittenOne =
+  `<li class="card">
              <article>
                <img
                  class="card_img"
@@ -26,14 +26,14 @@ const kittens = document.querySelector(".js-list");
              </article>
          </li>`;
 
-        const kittenImage2 = "https://dev.adalab.es/sphynx-gato.webp";
-        const kittenName2 = "Fiona";
-        const kittenDesc2 =
-           " Produce fascinación y curiosidad. Exótico, raro, bello, extraño hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.";
-         const kittenRace2 = "Sphynx"; 
+const kittenImage2 = "https://dev.adalab.es/sphynx-gato.webp";
+const kittenName2 = "Fiona";
+const kittenDesc2 =
+  " Produce fascinación y curiosidad. Exótico, raro, bello, extraño hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.";
+const kittenRace2 = "Sphynx";
 
-          const kittenTwo =
-           `<li class="card">
+const kittenTwo =
+  `<li class="card">
             <img
               class="card_img"
               src="${kittenImage2}"
@@ -46,13 +46,13 @@ const kittens = document.querySelector(".js-list");
             </p>
           </li>`;
 
-        const kittenImage3 = "https://dev.adalab.es/maine-coon-cat.webp";
-        const kittenName3 = "Cielo";
-        const kittenDesc3 =
-           " Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.";
-        const kittenRace3 = "Maine Coon"; 
+const kittenImage3 = "https://dev.adalab.es/maine-coon-cat.webp";
+const kittenName3 = "Cielo";
+const kittenDesc3 =
+  " Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.";
+const kittenRace3 = "Maine Coon";
 
-        const kittenThree = `<li class="card">
+const kittenThree = `<li class="card">
             <img
               class="card_img"
               src=${kittenImage3}
@@ -66,36 +66,67 @@ const kittens = document.querySelector(".js-list");
           </li>`;
 
 
-        kittens.innerHTML = kittenOne + kittenTwo + kittenThree;
+kittens.innerHTML = kittenOne + kittenTwo + kittenThree;
 
-    
-  
-    
-    const menuAdd = document.querySelector('.js-btn-add');
-    const newForm = document.querySelector('.newForm');
+const menuAdd = document.querySelector('.js-btn-add');
+const newForm = document.querySelector('.newForm');
 
-      function showNewCatForm () {
-        newForm.classList.remove('collapsed');
-        console.log(newForm)}
-    
-      function hideNewCatForm() {
-        newForm.classList.add('collapsed');
-      }
+function showNewCatForm() {
+  newForm.classList.remove('collapsed');
+  console.log(newForm)
+}
 
-    newForm.addEventListener("click", showNewCatForm);
-   
-    
-    //   formSection.classList.remove('collapsed');
-    // });
+function hideNewCatForm() {
+  newForm.classList.add('collapsed');
+}
 
-    // const buttonCancel = document.querySelector('.js-btn-cancel');
+newForm.addEventListener("click", showNewCatForm);
 
-    // buttonCancel.addEventListener("click", () => {
-    //   const formSection = document.querySelector('.new-form');
-    //   formSection.classList.add('collapsed');
-    //   // console.log("clickcancel");
-    // });
 
+//   formSection.classList.remove('collapsed');
+// });
+
+// const buttonCancel = document.querySelector('.js-btn-cancel');
+
+// buttonCancel.addEventListener("click", () => {
+//   const formSection = document.querySelector('.new-form');
+//   formSection.classList.add('collapsed');
+//   // console.log("clickcancel");
+// });
+
+/* 
+ Cuando la usuaria haga click en buscar 
+ - recogemos el valor de la descripción 
+  - si la descripción de Anastasio incluye lo que ha puesto la usuaria
+    yo mostraré a Anastasio
+  - si la descripción de gato2 incluye lo que ha puesto la usuaria
+    yo mostraré a gato2
+  - si la descripción de gato3 incluye lo que ha puesto la usuaria
+    yo mostraré a gato3
+  Elementos html que necesitamos:
+  La descripción y el botón
+*/
+
+const buttonSearch = document.querySelector('.js-button-search');
+const input_search_desc = document.querySelector('.js-desc');
+
+const handleClick = (event) => {
+  event.preventDefault();
+const descrSearchText = input_search_desc.value; 
+console.log("Ha hecho click");
+};
+
+const filterKitten = (event) => {
+  if (kittenDesc1.includes(descrSearchText)) {
+    listElement.innerHTML += kittenOne;
+  }
+  if (kittenDesc2.includes(descrSearchText)) {
+    listElement.innerHTML += kittenTwo;
+  }
+  if (kittenDesc3.includes(descrSearchText)) {
+    listElement.innerHTML += kittenThree;
+  }
+};
  
+buttonSearch.addEventListener('click', handleClick);
 
-      
